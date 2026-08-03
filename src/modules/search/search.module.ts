d973dbe29e1f4f6e817c79ed85282be74cb59d77
Kunal Rana <kunalranarj2005@gmail.com>
2026-08-03 12:48:19 +0530

    fixed build issue

--- src/modules/search/search.module.ts
+++ src/modules/search/search.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from '../../prisma/prisma.service';
 import { SearchResolver } from './search.resolver';
 import { SearchService } from './search.service';
 
 @Module({
-  providers: [SearchResolver, SearchService],
+  providers: [SearchResolver, SearchService, PrismaService],
 })
 export class SearchModule {}
